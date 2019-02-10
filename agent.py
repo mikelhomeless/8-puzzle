b00ff50eaa55bbcb81238026d107735621bf0815
mikelhomeless <mthomer1027@gmail.com>
2019-02-10 00:45:00 -0600

    added file I/O

--- agent.py
+++ agent.py
@@ -6,29 +6,63 @@ class EightPuzzleAgent:
         self.__explored_states = set()
 
     def solve(self):
+        """Obtain the optimal solution for EightPuzzle if it exists
+
+        For each node/state explored, check if the current board is solved and return its history.
+        If the puzzle is not yet solved, locate the next set of possible moves and push them into the
+        priority queue (heap).
+
+        If the queue is emptied, all possible states have been explored and no solution exists
+        """
         self.queue_moves(self.__game.get_available_moves(), '')
         while self.__queue:
-            search_node = heappop(self.__queue)
-            self.__game.set_gamestate(*search_node[1])
+            priority, game_state, history = heappop(self.__queue)
+            self.__game.set_gamestate(*game_state)
             if self.__game.is_solved():
-                return search_node[2]
-            self.queue_moves(self.__game.get_available_moves(), search_node[2])
+                return history
+            self.queue_moves(self.__game.get_available_moves(), history)
+
         return False
 
-    # nodes are a list with the following form [priority, state - tuple(board, empt tile position), history - str ]
     def queue_moves(self, moves_list, move_history):
+        """Push potential moves into the queue to be explored
+        moves_list -- [(direction, (resulting_board_state, blank_position)),
+                       (direction, (resulting_board_state, blank_position)), ... ]
+        move_history -- string of all moves made on the board prior to the ones being added
+
+        Go through every move in the provided list and check if the resulting board configuration has been seen before.
+        If board is so far unique, add it to the set of explored states, then add the game state and move history to the queue
+        """
         for item in moves_list:
             move, game_state = item
             if str(game_state[0]) not in self.__explored_states:
                 self.__explored_states.add(str(game_state[0]))
-                updated_history = move_history + move
-                priority = len(move_history) + self.manhattan_distance(game_state[0])
-                heappush(self.__queue, (priority, game_state, updated_history))
+                heappush(self.__queue, self.create_node(move, game_state, move_history))
 
     def manhattan_distance(self, arr):
-        return sum([self.mhd(index, num - 1) for index, num in enumerate(arr)])
+        """
+        Returns the manhattan distane of an entire array
+        """
+        return sum([self.mhd(index, num - 1) for index, num in enumerate(arr) if num != 9])
 
     def mhd(self, index, num):
-    	cur_row, cur_col = index // 3, index % 3
-    	goal_row, goal_col = num // 3, num % 3
-    	return abs(goal_row - cur_row) + abs(goal_col - cur_col)
+        """Calculate the manhattan distance of an individual square
+        index - the index the square is currently located in the array
+        num - the value of the square (The position it should end up in the goal state)
+
+        returns an integer
+        """
+        cur_row, cur_col = index // 3, index % 3
+        goal_row, goal_col = num // 3, num % 3
+        return abs(goal_row - cur_row) + abs(goal_col - cur_col)
+
+    def create_node(self, move, game_state, history):
+        """Create a node to be placed in the Priority Queue
+        Returns a list with the following form [priority: int, state: tuple(board, empty tile position), history: str ]
+        """
+        # update the history to include the new move
+        updated_history = history + move
+
+        # Calculate priority based of manhattan_distance + moves made so far
+        priority = len(history) + self.manhattan_distance(game_state[0])
+        return priority, game_state, updated_history
--- game.py
+++ game.py
@@ -26,13 +26,18 @@ class EightPuzzle:
         return self.__board.index(9)
 
     def __init__(self, board):
-        self.__board = [9 if x == 'E' else x for x in board]
+        self.__board = board
         self.__blank_index = self.__find_index_of_blank()
 
     def is_valid_move(self, direction):
         return direction in self.VALID_MOVES[self.__blank_index]
 
     def move(self, direction):
+        """move the blank space in the direction that was given
+        direction -- char (See MOVES class variable)
+
+        Returns a tuple of game state resulting from the move (board, blank_index)
+        """
         if self.is_valid_move(direction):
             self.__swap(self.MOVES[direction])
             return self.__board[:], self.__blank_index
@@ -40,22 +45,37 @@ class EightPuzzle:
             raise InvalidMoveError(direction)
 
     def set_gamestate(self, board, empty_pos=None):
-        self.__board = [9 if x == 'E' else x for x in board]
+        """Manually set/change the state of the game
+        board: array of integers, index is the integer's current position on the board
+        empty_pos: location of the empty square. found automatically if not specified
+        """
+        self.__board = board
         self.__blank_index = self.__find_index_of_blank() if empty_pos == None else empty_pos
 
     def is_solved(self):
         return all([a < b for a,b in zip(self.__board, self.__board[1:])])
 
     def get_available_moves(self):
+        """Return the set of moves that can be made in game's current state
+
+        Returns a list of tuples of the following form (move: char, potential_game_state: tuple(potential_board, potential blank_index))
+        """
         return [(k, self.peak(k)) for k in self.VALID_MOVES[self.__blank_index]]
 
     def peak(self, direction):
+        """Test what the board would look like if a given move is made
+        direction: char
+
+        returns the potential board_state as a tuple (board, blank_index)
+        """
         future_state = self.move(direction)
         self.__swap(-self.MOVES[direction]) # reset board to its previous configuration
         return future_state
 
-
     def __swap(self, distance):
+        """Swap the index of the blank square with an index x indecies away
+        distance: int - distance of away the square to swap with is
+        """
         old = self.__blank_index
         self.__blank_index += distance
         self.__board[old], self.__board[self.__blank_index] = self.__board[self.__blank_index], self.__board[old]
